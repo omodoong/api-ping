be827311572be4c0559000179fdf84a39fcdab7d
Oki Marzuqi <33563037+omodoong@users.noreply.github.com>
2018-02-17 09:46:48 -0800

    Add files via upload

---
+++ split.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "net/http"
+    "log"
+    "time"
+
+    "github.com/gorilla/mux"
+)
+
+type Todo struct {
+    Name      string    `json:"name"`
+    Completed bool      `json:"completed"`
+    Due       time.Time `json:"due"`
+}
+
+type Route struct {
+    Name        string
+    Method      string
+    Pattern     string
+    HandlerFunc http.HandlerFunc
+}
+
+type Todos []Todo
+type Routes []Route
+
+func NewRouter() *mux.Router {
+
+    router := mux.NewRouter().StrictSlash(true)
+    for _, route := range routes {
+        router.
+            Methods(route.Method).
+            Path(route.Pattern).
+            Name(route.Name).
+            Handler(route.HandlerFunc)
+    }
+
+    return router
+}
+
+var routes = Routes{
+    Route{
+        "Index",
+        "GET",
+        "/",
+        Index,
+    },
+    Route{
+        "TodoIndex",
+        "GET",
+        "/todos",
+        TodoIndex,
+    },
+    Route{
+        "TodoShow",
+        "GET",
+        "/todos/{todoId}",
+        TodoShow,
+    },
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Welcome!")
+}
+
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+    todos := Todos{
+        Todo{Name: "Write presentation"},
+        Todo{Name: "Host meetup"},
+    }
+
+    w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+    w.WriteHeader(http.StatusOK)
+    if err := json.NewEncoder(w).Encode(todos); err != nil {
+        panic(err)
+    }
+}
+
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    todoId := vars["todoId"]
+    fmt.Fprintln(w, "Todo show:", todoId)
+}
+
+func main() {
+
+    router := NewRouter()
+
+    log.Fatal(http.ListenAndServe(":8080", router))
+}
