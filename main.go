462dde977e386239ee96429fca4c12c228215597
Oki Marzuqi <33563037+omodoong@users.noreply.github.com>
2018-02-28 03:38:14 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,16 +1,23 @@
 package main
 
 import(
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"log"
 	"os"
-	"os/exec"
-	"strings"
 	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/sparrc/go-ping"
 )
+
+type Category struct {
+	Rtts []string		`json: "rtts_latency"`
+    Avg  string			`json: "average_latency"`
+    Max string			`json: "maximum_latency"`
+    Min  string			`json: "average_latency"`
+}
 
 var timeoutSec int
 var message    string
@@ -18,32 +25,53 @@ var logFile *os.File
 
 func Getping(w http.ResponseWriter, r *http.Request) {
 	dest := r.FormValue("dest")
-	fmt.Println("destionation : ", dest)
-	fmt.Fprintln(w, "Report Connection...")
-	fmt.Fprint(w, "destionation : ")
-	w.Write([]byte(dest))
-
-	out, _ := exec.Command("ping", dest, "-c 3", "-i 3", "-w 3").Output()
-	
-	if strings.Contains(string(out), "Destination Host Unreachable") {
-    	fmt.Println(time.Now().Format("2006-01-02 15:04:05"), "Status No Connected")
-    	fmt.Fprintln(w, "")
-    	fmt.Fprintln(w, "Status Not Connected")
-		} else {
-    		fmt.Println(time.Now().Format("2006-01-02 15:04:05"), "Status Connected")
-    		fmt.Fprintln(w, "")
-    		fmt.Fprintln(w, "Status Connected")
-
+	pinger, err := ping.NewPinger(dest)
+	pinger.SetPrivileged(true)
+	pinger.Timeout = 3 * time.Second
+	if err != nil {
+	    w.Write([]byte(err.Error()))
 	}
-	fmt.Fprintln(w, "time :", time.Now().Format("Mon Jan 2 15:04:05 MST 2006"))		
+	pinger.Count = 3
+	pinger.Run() // blocks until finished
+	stats := pinger.Statistics() // get send/receive/rtt stats
+	fmt.Println(stats)
+
+	avg := stats.AvgRtt
+	Savg := fmt.Sprint(avg)
+    max := stats.MaxRtt
+    Smax := fmt.Sprint(max)
+    min := stats.MinRtt
+    Smin := fmt.Sprint(min)
+    pack := stats.PacketLoss
+
+
+    // masukkan rtts dalam bentuk []time.duration ke rtts dalam bentuk []string
+    rttsString := []string{}
+    for _, rtt := range(stats.Rtts) {
+    	rttsString = append(rttsString, rtt.String())
+    }
+
+    // Create an instance of the Box struct.
+    category := Category{
+    	Rtts: rttsString,
+        Avg: Savg,
+        Max: Smax,
+        Min: Smin,
+    }
+    // Create JSON from the instance data.
+    // ... Ignore errors.
+    b, _ := json.MarshalIndent(category, "", "    ")
+    // Convert bytes to string.
+    // s := string(b)
+    // fmt.Println(s)
+	w.Write(b)	
+
 }
 
 func main() {
-
 	r := mux.NewRouter()
 	// List of Routes
 	r.HandleFunc("/ping", Getping).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", "8080"), r))
-
 }
